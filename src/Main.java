0568faea4cee9e50a3cc52bae34987d37af15159
Ivtus <qq819307625@gmail.com>
2023-11-09 23:45:15 +0900

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,13 @@ import java.security.cert.CertificateException;
 public class Main {
     public static void main(String[] args) throws CommitException, GatewayException, CertificateException, IOException, InvalidKeyException, InterruptedException {
         App app = new App();
-//        app.Invoke("SetRate", new String[]{"0.245"});
+//        app.Invoke("SetRate", new String[]{"0.221"});
 //        app.Query("GetRate", new String[]{});
-        app.Invoke("CreateAsset", new String[]{"asset3","Maybe","100","0"});
+//        app.Invoke("CreateAsset", new String[]{"asset1","Maybe","100","0"});
 //        app.Query("ReadAsset", new String[]{"asset1"});
-//        app.Invoke("TransferPoint", new String[]{"asset1","1"});
+//        app.Invoke("TransferPoint", new String[]{"asset1","200"});
 //        app.Invoke("DeleteAsset", new String[]{"asset3"});
+//        app.Invoke("UpdateAsset",new String[]{"asset4","Maybe","150","0"});
+//        app.Invoke("DeleteAsset",new String[]{"asset3"});
     }
 }
