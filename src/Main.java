f79cc5ee6325b0aac52ff86f6141bf0eede02a70
Ivtus <qq819307625@gmail.com>
2023-11-12 23:54:44 +0900

    version 4.0

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args) throws CommitException, GatewayException, CertificateException, IOException, InvalidKeyException, InterruptedException {
         App app = new App();
 //        app.Invoke("SetRate", new String[]{"0.221"});
-        app.Query("GetRate", new String[]{});
-//        app.Invoke("CreateAsset", new String[]{"asset1","Maybe","100","0"});
+//        app.Query("GetRate", new String[]{});
+        app.Invoke("CreateAsset", new String[]{"asset1","Maybe","100","0"});
 //        app.Query("ReadAsset", new String[]{"asset1"});
 //        app.Invoke("TransferPoint", new String[]{"asset1","200"});
 //        app.Invoke("DeleteAsset", new String[]{"asset3"});
